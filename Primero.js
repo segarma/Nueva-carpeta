c459823e4dd8c67c65f663bed4bab03a76efaf15
Sergio <segarma@hotmail.com>
2024-06-01 22:54:03 +0200

    inicio

---
+++ Primero.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt-sync')();
+let numero1 = parseInt(prompt('Escribe el primer número: '));
+let numero2 = parseInt(prompt('Escribe el segundo número: '));
+let numero3 = parseInt(prompt('Escribe el tercer número: '));
+let numero4 = parseInt(prompt('Escribe el cuarto número: '));
+let resultado1 = ((numero1 * numero2) + numero3 / numero4).toFixed(2);
+let resultado4 = Math.trunc((numero1 * numero2) + numero3 / numero4);
+let resultado2 = (numero1 * (numero2 + numero3 / numero4)).toFixed(2);
+let resultado3 = (numero1 * (numero2 + numero3) / numero4).toFixed(2);
+console.log("Operación: (%s * %s) + %s / %s =%s",numero1,numero2,numero3,numero4,resultado1);
+console.log("Operación: %s * (%s + %s / %s) =%s",numero1,numero2,numero3,numero4,resultado2);
+console.log("Operación: %s * (%s + %s) / %s =%s",numero1,numero2,numero3,numero4,resultado4);
